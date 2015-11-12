be42d750e6ceaa29c6a67950d406aa9884b1f421
Toni Gurski <tg@mandarin-medien.de>
2015-11-12 16:43:44 +0100

    Set up a proper defaultController route

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,15 +5,13 @@ namespace MandarinMedien\MMMediaBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+
+        return new Response('Hallo');
     }
 }
