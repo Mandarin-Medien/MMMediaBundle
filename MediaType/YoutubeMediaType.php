9b3f9ee720868e89751218ef5f4bc16fc9bdd4b8
Toni Gurski <tg@mandarin-medien.de>
2015-11-23 15:32:53 +0100

    MediaType: implemented a new type to handle youtube urls YoutubeMediaType: thx 2 SoantaMediaBundle

--- MediaType/YoutubeMediaType.php
+++ MediaType/YoutubeMediaType.php
@@ -9,12 +9,68 @@
 namespace MandarinMedien\MMMediaBundle\MediaType;
 
 
+use MandarinMedien\MMMediaBundle\Model\MediaInterface;
+
 class YoutubeMediaType extends BaseMediaType
 {
     const NAME = 'mm.media.type.youtube';
 
+    private $loadedMetaData;
+
     static function check($data)
     {
-        return new self($data);
+        dump($data);
+
+        if(!($id = self::checkYoutubeLink($data)))
+            return;
+
+        return new self($id);
     }
+
+    public function getMetaData()
+    {
+        if($this->loadedMetaData)
+        {
+           return $this->loadedMetaData;
+        }
+        $url = sprintf('http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=%s&format=json', $this->getReference());
+        $data = file_get_contents($url);
+        $metadata = json_decode($data,true);
+
+        return $metadata ;
+    }
+
+    private static function checkYoutubeLink($link)
+    {
+        if (strlen($link) === 11) {
+            return false;
+        }
+        if (preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\#\?&\"'>]+)/", $link, $matches)) {
+            return $matches[1];
+        }
+
+        return false;
+    }
+
+    /**
+     * @param MediaInterface $media
+     * @param array|null $options
+     * @return string
+     */
+    public function getPreview(MediaInterface $media, array $options = null)
+    {
+        dump($media->getMediaTypeMetadata() );
+        return $media->getMediaTypeMetadata()['html'];
+    }
+
+    /**
+     * get the media name
+     *
+     * @return string
+     */
+    public function getMediaName()
+    {
+        $this->getMetaData()['title'];
+    }
+
 }
