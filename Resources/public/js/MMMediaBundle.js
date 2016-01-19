29e49c48b2210f23675dfbde85ae145d724ece4e
Toni Gurski <tg@mandarin-medien.de>
2016-01-19 12:56:37 +0100

    Added Multilanguage support

--- Resources/public/js/MMMediaBundle.js
+++ Resources/public/js/MMMediaBundle.js
@@ -44,7 +44,7 @@ Dropzone.autoDiscover = false;
  *
  * @constructor
  */
-function MMMediaBundleFileDropzone(_id, _url, _fieldName, _multiple, _files) {
+function MMMediaBundleFileDropzone(_id, _url, _fieldName, _multiple, _files,_options) {
 
     var $this = this;
 
@@ -79,112 +79,131 @@ function MMMediaBundleFileDropzone(_id, _url, _fieldName, _multiple, _files) {
             if ($reference)
                 _file.previewElement.appendChild($reference);
         }
-    }
+    };
 
-    var $id_dropzone_preview = _id + '_previews'
+    /**
+     * function which get called by Dropzone after init
+     */
+    this.initCallback = function () {
+
+        var myDropzone = this;
+
+        /**
+         * checks if its an mutiple field and adds the related event
+         */
+        if (_multiple) {
+
+            /**
+             * hooks the successmultiple event to append all hidden inputs which holds all tthe entity ids
+             */
+            myDropzone.on("successmultiple", function (files, response) {
+
+
+                if (response && response.success) {
+
+                    for (file in response.data) {
+
+                        files[file].id = response.data[file].id;
+
+                        $this.appendHiddenEntityInput(files[file]);
+
+                    }
+
+                }
+
+            });
+        }
+        else {
+            /**
+             * hooks the sucess event to append a hidden input which holds the entity id
+             */
+            myDropzone.on("success", function (file, response) {
+
+                if (response && response.success) {
+
+                    for (tFile in response.data) {
+
+                        file.id = response.data[0].id;
+
+                        $this.appendHiddenEntityInput(file);
+                    }
+                }
+            });
+        }
+
+
+        /**
+         * add already stored files to the widget
+         */
+
+            //temporaty add this event to get the real formated dropzone-file objects
+        myDropzone.on("addedfile", $this.appendHiddenEntityInput);
+
+        //loops thought the presetted files and appends them to the dropzone_preview
+        for (var i = 0; i < _files.length; i++) {
+            var mock = _files[i];
+            mock.accepted = true;
+
+            myDropzone.files.push(mock);
+            myDropzone.emit('addedfile', mock);
+            myDropzone.createThumbnailFromUrl(mock, mock.url);
+            myDropzone.emit('complete', mock);
+        }
+
+        //removes the temporaty added this event
+        myDropzone.off("addedfile", $this.appendHiddenEntityInput);
+
+        /**
+         * loads Dragular to enable drag and drop functionality
+         * @TODO: check if its possible to enable drag n drop with multiple instances of Dropzone
+         */
+        if (_multiple) {
+
+            var $dragula = dragula([document.querySelector($id_dropzone_preview)],
+                {
+                    direction: 'horizontal',
+                    mirrorContainer: document.querySelector($id_dropzone_preview)
+
+                });
+        }
+    };
+
+    /**
+     * dom id of preview container
+     * @type {string}
+     */
+    var $id_dropzone_preview = _id + '_previews';
+
+    var $options = {
+        url: _url
+        // The configuration we've talked about above
+        , autoProcessQueue: true
+        , paramName: (_multiple ? 'files' : 'files[]')
+        , addRemoveLinks: true
+
+        , previewsContainer: $id_dropzone_preview
+
+        // multiple settings
+        , uploadMultiple: _multiple
+        , parallelUploads: ( (_multiple) ? 100 : 1 )
+        , maxFiles: ( (_multiple) ? 100 : 1 )
+
+        // The setting up of the dropzone
+        , init: this.initCallback
+    };
+
+
+    /**
+     * merge aditional given options
+     */
+    if (typeof _options == "object")
+        for (var key in _options)
+            $options[key] = _options[key];
 
     /**
      * loads the Dropzone instance
      */
-    this.dropzone = new Dropzone(_id,
-        { // The camelized version of the ID of the form element
-            url: _url,
-            // The configuration we've talked about above
-            autoProcessQueue: true,
-            paramName: (_multiple ? 'files' : 'files[]'),
-            addRemoveLinks: true,
-            previewsContainer: $id_dropzone_preview,
-
-            uploadMultiple: _multiple,
-            parallelUploads: ( (_multiple) ? 100 : 1 ),
-            maxFiles: ( (_multiple) ? 100 : 1 ),
-
-            // The setting up of the dropzone
-            init: function () {
-
-                var myDropzone = this;
-
-                /**
-                 * checks if its an mutiple field and adds the related event
-                 */
-                if (_multiple) {
-
-                    /**
-                     * hooks the successmultiple event to append all hidden inputs which holds all tthe entity ids
-                     */
-                    myDropzone.on("successmultiple", function (files, response) {
-
-
-                        if (response && response.success) {
-
-                            for (file in response.data) {
-
-                                files[file].id = response.data[file].id;
-
-                                $this.appendHiddenEntityInput(files[file]);
-
-                            }
-
-                        }
-
-                    });
-                }
-                else {
-                    /**
-                     * hooks the sucess event to append a hidden input which holds the entity id
-                     */
-                    myDropzone.on("success", function (file, response) {
-
-                        if (response && response.success) {
-
-                            for (tFile in response.data) {
-
-                                file.id = response.data[0].id;
-
-                                $this.appendHiddenEntityInput(file);
-                            }
-                        }
-                    });
-                }
-
-
-                /**
-                 * add already stored files to the widget
-                 */
-
-                    //temporaty add this event to get the real formated dropzone-file objects
-                myDropzone.on("addedfile", $this.appendHiddenEntityInput);
-
-                //loops thought the presetted files and appends them to the dropzone_preview
-                for (var i = 0; i < _files.length; i++) {
-                    var mock = _files[i];
-                    mock.accepted = true;
-
-                    myDropzone.files.push(mock);
-                    myDropzone.emit('addedfile', mock);
-                    myDropzone.createThumbnailFromUrl(mock, mock.url);
-                    myDropzone.emit('complete', mock);
-                }
-
-                //removes the temporaty added this event
-                myDropzone.off("addedfile", $this.appendHiddenEntityInput);
-
-                /**
-                 * loads Dragular to enable drag and drop functionality
-                 * @TODO: check if its possible to enable drag n drop with multiple instances of Dropzone
-                 */
-                if (_multiple) {
-
-                    var $dragula = dragula([document.querySelector($id_dropzone_preview)],
-                        {
-                            direction: 'horizontal',
-                            mirrorContainer: document.querySelector($id_dropzone_preview)
-
-                        });
-                }
-            }
-        }
-    );
+    this.dropzone = new Dropzone(_id, $options);
 }
 
 function MMMediaBundleFileDropzoneInitiateEvents() {
@@ -221,8 +240,23 @@ MMMediaBundleDomReady(function (event) {
         else
             multiple = true;
 
+        var _options = {
+            dictDefaultMessage: dropzone.getAttribute('data-dictDefaultMessage'), // The message that gets displayed before any files are dropped. This is normally replaced by an image but defaults to "Drop files here to upload"'
+            dictFallbackMessage: dropzone.getAttribute('data-dictFallbackMessage'), // If the browser is not supported, the default message will be replaced with this text. Defaults to "Your browser does not support drag'n'drop file uploads."'
+            dictFallbackText: dropzone.getAttribute('data-dictFallbackText'), // This will be added before the file input files. If you provide a fallback element yourself, or if this option is null this will be ignored. Defaults to "Please use the fallback form below to upload your files like in the olden days."'
+            dictInvalidFileType: dropzone.getAttribute('data-dictInvalidFileType'), // Shown as error message if the file doesn·'t match the file type.'
+            dictFileTooBig: dropzone.getAttribute('data-dictFileTooBig'), // Shown when the file is too big. {{filesize}} and {{maxFilesize}} will be replaced.'
+            dictResponseError: dropzone.getAttribute('data-dictResponseError'), // Shown as error message if the server response was invalid. {{statusCode}} will be replaced with the servers status code.'
+            dictCancelUpload: dropzone.getAttribute('data-dictCancelUpload'), // If addRemoveLinks is true, the text to be used for the cancel upload link.'
+            dictCancelUploadConfirmation: dropzone.getAttribute('data-dictCancelUploadConfirmation'), // If addRemoveLinks is true, the text to be used for confirmation when cancelling upload.'
+            dictRemoveFile: dropzone.getAttribute('data-dictRemoveFile'), // If addRemoveLinks is true, the text to be used to remove a file.'
+            dictRemoveFileConfirmation: dropzone.getAttribute('data-dictRemoveFileConfirmation'), //
+            dictMaxFilesExceeded: dropzone.getAttribute('data-dictMaxFilesExceeded')  // If maxFiles is set, this will be the error message when it\'s exceeded.
+        };
+
+
         //loads the UploadWidget-Dropzone
-        new MMMediaBundleFileDropzone("#" + id, url, fieldName, multiple, files);
+        new MMMediaBundleFileDropzone("#" + id, url, fieldName, multiple, files, _options);
     }
 
     MMMediaBundleFileDropzoneInitiateEvents();
